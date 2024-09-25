0094ca01ff51e998df9bc011e90c4851f8d820f8
dealhouse <de.juan1@live.com>
2024-09-25 09:30:17 -0300

    created bookbot basic report features

---
+++ main.py
@@ -0,0 +1,24 @@
+with open("./books/frankenstein.txt", "r") as f:
+    # print(f.read())
+    words = f.read()
+    word_list = words.split()
+    # print(len(word_list))
+    lower = words.lower()
+    characters = list(lower)
+    character_list = {}
+    for character in characters:
+        if character.isalpha() and character not in character_list:
+            character_list[character] = 1
+        elif character.isalpha() and character in character_list: 
+            character_list[character] += 1
+    # print(character_list)
+    sorted_character_list = sorted(character_list.items(), key=lambda item:item[1], 
+                                reverse=True)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(word_list)} words found in the document")
+    print("")
+    for character in sorted_character_list:
+        print(f"The '{character[0]}' character was found {character[1]} times")
+    print("--- End report ---")
+    
+
